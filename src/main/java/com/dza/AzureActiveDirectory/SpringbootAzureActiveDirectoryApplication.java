29c87edfbddae04bc13c70984ef5140c20b35b02
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2019-12-16 15:24:39 -0500

    create Spring Boot security app use Azure Active Directory

---
+++ src/main/java/com/dza/AzureActiveDirectory/SpringbootAzureActiveDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.dza.AzureActiveDirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAzureActiveDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAzureActiveDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dza/AzureActiveDirectory/SpringbootAzureActiveDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dza.AzureActiveDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAzureActiveDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
